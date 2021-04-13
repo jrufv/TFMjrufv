d3f7e41264a6862598d08cd9e796a22f80d17899
Jordi Ruf Vilches <jrufv@uoc.edu>
2021-04-13 16:25:05 +0200

    Confirmación inicial

---
+++ R/1_lectura_de_datos.R
@@ -0,0 +1,177 @@
+#' Lectura de archivos de datos ómicos
+#'
+#' Lectura de diferentes tipos de archivos de datos ómicos y construcción de un
+#' objeto en el que almacenar los datos de los ensayos y las muestras.
+#' @param data_type Tipo de datos a introducir, puede ser \code{"microarray"},
+#'   \code{"RNA-Seq"}, \code{"MetabRS"}, \code{"MetabSB"} o \code{"MetabMC"}.
+#' @param path Ruta de ubicación de los archivos para \code{data_type =
+#'   "microarray"} o \code{"MetabRS"}.
+#' @param file_type Tipo de archivos,  para \code{data_type = "MetabRS"}. Puede
+#'   ser \code{".NetCDF"}, \code{".mzML"}, \code{".mzXML"} o \code{".mzData"}.
+#' @param raw_data Ruta y nombre del archivo con los datos brutos del
+#'   experimento, para \code{data_type = "RNA-Seq"}, \code{"MetabSB"} o
+#'   {"MetabMC"}. Debe ser un archivo \code{.csv} o \code{.txt} con cabecera. La
+#'   primera columna debe corresponder a los genes (\code{"RNA-Seq"}), los bins
+#'   (\code{"MetabSB"}) o los metabolitos (\code{"MetabMC"}) y las siguientes a
+#'   las muestras.
+#' @param sep_rd Carácter separador de campo para el archivo \code{raw_data}.
+#'   Los valores de cada línea del archivo están separados por este carácter. De
+#'   forma predeterminada \code{sep_rd = ""}.
+#' @param targets Ruta y nombre del archivo con la información sobre las
+#'   muestras del experimento. Debe ser un archivo \code{.csv} o \code{.txt} con
+#'   cabecera. Para \code{data_type = "microarray"} o \code{"MetabRS"} debe
+#'   tener tres columnas como mínimo, la primera con el nombre de los archivos
+#'   que contienen los datos de cada muestra, la segunda con un identificador
+#'   único de las muestras y la tercera con el grupo experimental de las
+#'   muestras. Para el resto debe tener dos columnas como mínimo, la primera con
+#'   el identificador de las muestras y la segunda con el grupo experimental de
+#'   las muestras. En ambos casos se aceptan más columnas como covariables.
+#' @param sep_targ Carácter separador de campo para el archivo \code{targets}.
+#'   Los valores de cada línea del archivo están separados por este carácter. De
+#'   forma predeterminada \code{sep_targ = ""}.
+#' @param mode Puede ser \code{"inMemory"} (predeterminado) o \code{"onDisk"}.
+#'   El primero carga los datos sin procesar en la memoria, mientras que el
+#'   segundo solo genera el objeto y se accede a los datos sin procesar en el
+#'   disco cuando es necesario (sólo válido para \code{data_type = "MetabRS"}).
+#' @return Un objeto de la clase \code{ExpressionFeatureSet} (para microarrays),
+#'   \code{DGEList} (para RNA-Seq), \code{OnDiskMSnExp} (para espectros brutos
+#'   de GC/LC-MS), \code{SummarizedExperiment} (para contenedores de espectros
+#'   de MS/NMR), o \code{MSnSet} (para concentraciones de metabolitos) que
+#'   contiene los datos de los ensayos y datos sobre las muestras.
+#' @export
+#' @examples
+#' # Para microarrays
+#' data_microarray <- read_data(data_type = "microarray",
+#'                              path = "./data/microarray",
+#'                              targets = "./data/microarray/targets.csv",
+#'                              sep_targ = ";")
+#' data_microarray
+#'
+#' # Para RNA-Seq
+#' data_RNASeq <- read_data(data_type = "RNA-Seq",
+#'                          raw_data = "./data/RNA-Seq/counts.csv",
+#'                          sep_rd = "\t",
+#'                          targets = "./data/RNA-Seq/targets.csv",
+#'                          sep_targ = ";")
+#' data_RNASeq
+#'
+#' # Para Espectros Brutos de GC/LC-MS
+#' data_MetabRS <- read_data(data_type = "MetabRS",
+#'                           path = "./data/Met_mzML",
+#'                           file_type = ".mzML",
+#'                           targets = "./data/Met_mzML/targets.csv",
+#'                           sep_targ = ";",
+#'                           mode = "onDisk")
+#' data_MetabRS
+#'
+#' # Para Contenedores de Espectros de MS/NMR
+#' data_MetabSB <- read_data(data_type = "MetabSB",
+#'                           raw_data = "./data/Met_spectra_bins/spectra.csv",
+#'                           sep_rd = ",",
+#'                           targets = "./data/Met_spectra_bins/targets.csv",
+#'                           sep_targ = ",")
+#' data_MetabSB
+#'
+#' # Para Concentraciones de Metabolitos
+#' data_MetabMC <- read_data(data_type = "MetabMC",
+#'                          raw_data = "./data/Met_conc/concent.csv",
+#'                          sep_rd = ",",
+#'                          targets = "./data/Met_conc/targets.csv",
+#'                          sep_targ = ",")
+#' data_MetabMC
+
+read_data <- function(data_type, path, file_type, raw_data, sep_rd = "",
+                      targets, sep_targ = "", mode = "inMemory") {
+
+  if(missing(data_type)) stop("argument data_type is missing, with no default")
+  if(data_type != "microarray" & data_type != "RNA-Seq" & data_type != "MetabRS" &
+     data_type != "MetabPL" & data_type != "MetabSB" & data_type != "MetabMC") {
+    stop("Data type must be microarray, RNA-Seq, MetabRS, MetabPL, MetabSB or MetabMC")
+  }
+
+  if(data_type == "microarray" | data_type == "MetabRS" | data_type == "MetabPL") {
+    if(missing(path)) stop("argument path is missing, with no default")
+
+    min_col = 3
+  }
+
+  if(data_type == "MetabRS") {
+    if(missing(file_type)) stop("argument file_type is missing, with no default")
+    if(file_type != ".NetCDF" & file_type != ".mzML" &
+       file_type != ".mzXML" & file_type != ".mzData") stop("the file_type is wrong")
+  }
+
+  if(data_type == "RNA-Seq" | data_type == "MetabSB" | data_type == "MetabMC") {
+    if(missing(raw_data)) stop("argument counts is missing, with no default")
+
+    file <- read.table(raw_data, header = TRUE, row.names = 1, sep = sep_rd)
+
+    min_col = 2
+  }
+
+  if(missing(targets)) stop("argument targets is missing, with no default")
+
+  if(data_type != "microarray") {
+    sampleinfo <- read.table(targets, header = TRUE, sep = sep_targ,
+                             stringsAsFactors = TRUE)
+    if(ncol(sampleinfo) < min_col) {
+      stop("the number of columns of the targets object must be greater than 1")
+    }
+  }
+
+  if(data_type == "microarray") {
+
+    files <- oligoClasses::list.celfiles(path, full.names = TRUE)
+    sampleinfo <- Biobase::read.AnnotatedDataFrame(targets, header = TRUE,
+                                                   row.names = 1, sep = sep_targ)
+    min_col <- 2
+    if(ncol(sampleinfo) < min_col) {
+      stop("the number of columns of the targets object must be greater than 2")
+    }
+    data <- oligo::read.celfiles(files, phenoData = sampleinfo)
+    sampleinfo@data[,1] -> rownames(pData(data))
+    colnames(data) <- rownames(pData(data))
+
+  } else if(data_type == "RNA-Seq") {
+
+    colnames(file) <- sampleinfo[,2]
+    if(ncol(sampleinfo) == min_col+1) {
+      data <- edgeR::DGEList(file, group = sampleinfo[,3])
+    } else {
+      data <- edgeR::DGEList(file, group = sampleinfo[,3],
+                             samples = sampleinfo[,4:ncol(sampleinfo)])
+    }
+
+  } else if(data_type == "MetabRS") {
+
+    files <- list.files(path, recursive = TRUE, full.names = TRUE, pattern = file_type)
+    data <- MSnbase::readMSData(files = files,
+                                pdata = new("NAnnotatedDataFrame", sampleinfo),
+                                mode = mode)
+    sampleinfo[,2] -> rownames(pData(data))
+
+  } else if(data_type == "MetabPL") {
+
+    stop("en contstrucción")
+
+  } else if(data_type == "MetabSB") {
+
+
+    rdata <- S4Vectors::DataFrame(row.names = rownames(file))
+    cdata <- S4Vectors::DataFrame(sampleinfo[2:ncol(sampleinfo)],
+                                  row.names = sampleinfo[,1])
+    file <- list(file)
+    file <- S4Vectors::SimpleList(file)
+    data <- SummarizedExperiment::SummarizedExperiment(file, rowData = rdata,
+                                                       colData = cdata)
+
+  } else if(data_type == "MetabMC") {
+
+    file <- t(file)
+    data <- POMA::PomaMSnSetClass(target = sampleinfo, features = file)
+
+  }
+
+  return(data)
+
+}
---
+++ R/2_gráficos.R
@@ -0,0 +1,285 @@
+#' Gráficos
+#'
+#' Realiza diferentes tipos de gráficos en función del objeto introducido.
+#' Acepta objetos del tipo \code{ExpressionFeatureSet}, \code{DGEList},
+#' \code{OnDiskMSnExp}, \code{MSnExp}, \code{SummarizedExperiment} o
+#' \code{MSnSet}.
+#' @param object Objeto del tipo \code{ExpressionFeatureSet}, \code{DGEList},
+#'   \code{OnDiskMSnExp}, \code{MSnExp}, \code{SummarizedExperiment} o
+#'   \code{MSnSet}.
+#' @param plot Tipo de gráfico. Puede ser \code{"PCA"} para gráfico de
+#'   componentes principales, \code{"boxplot"} para diagramas de caja,
+#'   \code{"heatmap"} para un mapa de calor de la varianza de genes,
+#'   \code{"barplot"} para un gráfico de barras de las librerías usadas (sólo
+#'   para datos de RNA-Seq), \code{"BPC"} para un cromatograma de pico base
+#'   (sólo para espectros de GC/LC-MS), \code{"heatmapS"} para un mapa de calor
+#'   por muestras o \code{"density"} para un gráfico de densidad por muestra.
+#' @return Un gráfico distinto en función de la selección realizada.
+#' @export
+#' @examples
+#' plotTFM(data_microarray, plot = "boxplot")
+
+plotTFM <- function (object, plot) {
+
+  if(class(object) == "ExpressionFeatureSet") {
+    dat_mat <- Biobase::exprs(object)
+    Groups <- Biobase::pData(object)[[2]]
+    labels <- Biobase::pData(object)[[1]]
+    name <- Biobase::varLabels(Biobase::phenoData(object))[2]
+    lev <- levels(as.factor(Biobase::pData(object)[[2]]))
+    title <- "Microarrays"
+    value <- "Expression"
+  } else if(class(object) == "DGEList") {
+    dat_mat <- object$counts
+    Groups <- as.character(object$samples[[1]])
+    labels <- colnames(object$counts)
+    name <- colnames(object$samples)[1]
+    lev <- levels(object$samples[[1]])
+    title <- "RNA-Seq"
+    value <- "Counts"
+  } else if(class(object) == "OnDiskMSnExp" | class(object) ==  "MSnExp") {
+    dat_mat <- matrix(MSnbase::tic(object), ncol = nrow(Biobase::pData(object)))
+    colnames(dat_mat) <- rownames(Biobase::pData(object))
+    Groups <- Biobase::pData(object)[3][,1]
+    rownames(dat_mat) <- Biobase::featureNames(
+      Biobase::featureData(object))[1:(length(MSnbase::tic(object))/length(Groups))]
+    labels <- rownames(Biobase::pData(object))
+    name <- as.character(Biobase::pData(object)[2][,1])
+    lev <- levels(Biobase::pData(object)[[3]])
+    title <- "Raw Spectra"
+    value <- "Intensity"
+  } else if(class(object) == "SummarizedExperiment") {
+    dat_mat <- as.matrix(SummarizedExperiment::assay(object))
+    Groups <- as.character(SummarizedExperiment::colData(object)[[1]])
+    labels <- colnames(object)
+    name <- colnames(SummarizedExperiment::colData(object))
+    lev <- levels(as.data.frame(SummarizedExperiment::colData(object))[[1]])
+    title <- "Spectra Bins"
+    value <- "Intensity"
+  } else if(class(object) == "MSnSet") {
+    dat_mat <- Biobase::exprs(object)
+    Groups <- as.character(Biobase::pData(object)[[1]])
+    labels <- Biobase::sampleNames(Biobase::phenoData(object))
+    name <- Biobase::varLabels(Biobase::phenoData(object))
+    lev <- levels(Biobase::pData(object)[[1]])
+    title <- "Metabolite Concentration"
+    value <- "Concentration"
+  }
+
+  datacpm <- edgeR::cpm(dat_mat, log = TRUE)
+  meltdatacpm <- reshape::melt(datacpm)
+  meltdatacpm <- data.frame(meltdatacpm, condition = cond_vect(object))
+  meltdatacpm <- meltdatacpm[, -1]
+  names(meltdatacpm) <- c("samples", "value", "condition")
+
+  meltdata <- reshape::melt(dat_mat)
+  meltdata <- data.frame(meltdata, condition = cond_vect(object))
+  meltdata <- meltdata[, -1]
+  names(meltdata) <- c("samples", "value", "condition")
+
+  if(plot == "PCA") {
+
+    pcmat <- prcomp(t(dat_mat), scale=FALSE)
+    pcdata <- data.frame(pcmat$x)
+    loads <- round(pcmat$sdev^2/sum(pcmat$sdev^2)*100,1)
+
+    ggplot2::ggplot(pcdata, ggplot2::aes(x = PC1, y = PC2)) +
+      ggplot2::theme_bw() +
+      ggplot2::geom_hline(yintercept = 0, color = "gray70") +
+      ggplot2::geom_vline(xintercept = 0, color = "gray70") +
+      ggplot2::geom_point(ggplot2::aes(color = Groups)) +
+      ggplot2::coord_cartesian(xlim = c(min(pcmat$x[,1]) - 5, max(pcmat$x[, 1]) + 5)) +
+      ggrepel::geom_text_repel(ggplot2::aes(label = labels), size = 3) +
+      ggplot2::labs(x = c(paste("PC1", loads[1], "%")),
+                    y = c(paste("PC2", loads[2], "%"))) +
+      ggplot2::ggtitle(paste("PCA para: ", title, sep=" ")) +
+      ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5)) +
+      ggplot2::scale_color_manual(values = grDevices::topo.colors(ngroup(object)))
+
+  } else if(plot == "boxplot") {
+
+    ggplot2::ggplot(meltdatacpm, ggplot2::aes(x = samples, y = value,
+                                              fill = condition)) +
+      ggplot2::theme_bw() +
+      ggplot2::geom_boxplot(notch = TRUE) +
+      ggplot2::labs(x = "Samples", y = paste(value, "(cpm)", sep = " ")) +
+      ggplot2::ggtitle(paste("Boxplot para:", title, sep = " ")) +
+      ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5)) +
+      ggplot2::scale_fill_manual(values = grDevices::topo.colors(ngroup(object))) +
+      ggplot2::scale_x_discrete(limits = labels)
+
+  } else if(plot == "heatmap") {
+
+    var_rows <- apply(datacpm, 1, var)
+    if(nrow(datacpm) > 1000) n = 500 else n = nrow(datacpm) * 0.25
+    select_var <- names(sort(var_rows, decreasing = TRUE))[1:n]
+    datacpm <- datacpm[select_var,]
+    mypalette <- RColorBrewer::brewer.pal(11,"RdYlBu")
+    morecols <- grDevices::colorRampPalette(mypalette)
+
+    gplots::heatmap.2(datacpm,  scale = "row", col = rev(morecols(50)), trace = "none",
+                      ColSideColors = col_vect(object), cexCol = 1, srtCol = 0,
+                      adjCol = 0.5, main = paste("Heatmap para:", title, sep = " "),
+                      xlab = "Samples", ylab = "Compounds")
+
+  } else if(plot == "barplot") {
+
+    if(class(object) == "DGEList") {
+
+      datalibsize <- data.frame(samples = labels, lib.size = object$samples$lib.size,
+                                group = Groups)
+
+      ggplot2::ggplot(datalibsize,
+                      ggplot2::aes(x = samples, y = lib.size, fill = group)) +
+        ggplot2::theme_bw() +
+        ggplot2::geom_bar(stat = "identity", width = 0.5) +
+        ggplot2::geom_text(ggplot2::aes(label = lib.size), vjust = 1.5, color = "white",
+                           size = 3.5) +
+        ggplot2::labs(x = "Samples", y = "Library size") +
+        ggplot2::ggtitle("Tamaño de librería por muestra para: RNA-Seq") +
+        ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5)) +
+        ggplot2::scale_fill_manual(values = grDevices::topo.colors(ngroup(object)))
+
+    } else stop("Barplot is only valid for DGEList objects")
+
+  } else if(plot == "BPC") {
+
+    if(class(object) == "OnDiskMSnExp" | class(object) ==  "MSnExp") {
+
+      bpis <- MSnbase::chromatogram(object, aggregationFun = "max")
+      MSnbase::plot(bpis, col = col_vect(object))
+      legend("topright", fill = grDevices::topo.colors(ngroup(object)), legend = lev)
+
+    } else stop("BPC is only valid for OnDiskMSnExp objects")
+
+  } else if(plot == "heatmapS") {
+
+    cormat <- cor(dat_mat)
+    mypalette <- RColorBrewer::brewer.pal(11,"RdYlBu")
+    morecols <- grDevices::colorRampPalette(mypalette)
+
+    gplots::heatmap.2(cormat, col = rev(morecols(50)), trace = "none",
+                      ColSideColors = col_vect(object), cexCol = 1, cexRow = 1,
+                      srtCol = 0, adjCol = 0.5,
+                      main = paste("Heatmap de muestras para:", title, sep = " "),
+                      xlab = "Samples", dendrogram = "col")
+    legend("bottomleft", fill = grDevices::topo.colors(ngroup(object)), legend = lev)
+
+  } else if(plot == "density") {
+
+    ggplot2::ggplot(meltdata, ggplot2::aes(x = value, color = condition)) +
+      ggplot2::theme_bw() +
+      ggplot2::geom_density() +
+      ggplot2::labs(x = value, y = "Density") +
+      ggplot2::ggtitle(paste("Gráfico de densidad para:", title, sep = " ")) +
+      ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5)) +
+      ggplot2::scale_color_manual(values = grDevices::topo.colors(ngroup(object)))
+
+  }
+
+}
+
+#' Vector de colores
+#'
+#' Genera un vector de colores por muestra para objetos
+#' \code{ExpressionFeatureSet}, \code{DGEList}, \code{OnDiskMSnExp},
+#' \code{MSnExp}, \code{SummarizedExperiment} y \code{MSnSet}.
+#' @param object Objeto.
+#' @return vector de caracteres (colores).
+#' @export
+#' @examples
+#' col_vect(data_microarray)
+
+col_vect <- function(object) {
+  if(class(object) == "ExpressionFeatureSet") {
+    vector <- Biobase::pData(object)[[2]]
+    factor <- as.factor(vector)
+  } else if(class(object) == "DGEList") {
+    factor <- object$samples$group
+    vector <- as.character(factor)
+  } else if(class(object) == "OnDiskMSnExp" | class(object) ==  "MSnExp") {
+    factor <- Biobase::pData(object)[[3]]
+    vector <- as.character(factor)
+  } else if(class(object) == "SummarizedExperiment") {
+    factor <- SummarizedExperiment::colData(object)[[1]]
+    vector <- as.character(factor)
+  } else if(class(object) == "MSnSet") {
+    factor <- Biobase::pData(object)[[1]]
+    vector <- as.character(factor)
+  }
+
+  colors <- grDevices::topo.colors(length(levels(factor)))
+
+  for(i in 1:length(levels(factor))) {
+    for(j in 1:length(vector)) {
+      if(levels(factor)[i] == vector[j]) vector[j] <- colors[i]
+    }
+  }
+  return(vector)
+}
+
+#' Vector de condición
+#'
+#' Crear un vector con el grupo experimental para cada muestra y compuesto a
+#' partir de objetos \code{ExpressionFeatureSet}, \code{DGEList},
+#' \code{OnDiskMSnExp}, \code{MSnExp}, \code{SummarizedExperiment} o
+#' \code{MSnSet}.
+#' @param object Objeto.
+#' @return Vector de caracteres (condición experimental).
+#' @export
+#' @examples
+#' cond_vect(data_microarray)
+
+cond_vect <- function(object) {
+
+  if(class(object) == "ExpressionFeatureSet") {
+    datos <- Biobase::exprs(object)
+    grupos <- Biobase::pData(object)[2][,1]
+  } else if(class(object) == "DGEList") {
+    datos <- object$counts
+    grupos <- as.character(object$samples$group)
+  } else if(class(object) == "OnDiskMSnExp" | class(object) ==  "MSnExp") {
+    datos <- matrix(MSnbase::tic(object), ncol = nrow(Biobase::pData(object)))
+    colnames(datos) <- rownames(Biobase::pData(object))
+    grupos <- as.character(Biobase::pData(object)[3][,1])
+  } else if(class(object) == "SummarizedExperiment") {
+    datos <- SummarizedExperiment::assay(object)
+    grupos <- as.character(SummarizedExperiment::colData(object)[,1])
+  } else if(class(object) == "MSnSet") {
+    datos <- Biobase::exprs(object)
+    grupos <- as.character(Biobase::pData(object)[[1]])
+  }
+
+  condition <- c()
+  for(i in 1:ncol(datos)) {
+    condition <- c(condition, rep(grupos[i], nrow(datos)))
+  }
+  return(condition)
+}
+
+#' Grupos experimentales
+#'
+#' Da el Número de grupos experimentales de objetos \code{ExpressionFeatureSet},
+#' \code{DGEList}, \code{OnDiskMSnExp}, \code{MSnExp},
+#' \code{SummarizedExperiment} y \code{MSnSet}.
+#' @param object Objeto.
+#' @return Numérico (número de grupos experimentales).
+#' @export
+#' @examples
+#' ngroup(data_microarray)
+
+ngroup <- function(object) {
+  if(class(object) == "ExpressionFeatureSet") {
+    groups <- length(levels(as.factor(Biobase::pData(object)[[2]])))
+  } else if(class(object) == "DGEList") {
+    groups <- length(levels(object$samples[[1]]))
+  } else if(class(object) == "OnDiskMSnExp" | class(object) ==  "MSnExp") {
+    groups <- length(levels(Biobase::pData(object)[[3]]))
+  } else if(class(object) == "SummarizedExperiment") {
+    groups <- length(levels(as.data.frame(SummarizedExperiment::colData(object))[[1]]))
+  } else if(class(object) == "MSnSet") {
+    groups <- length(levels(Biobase::pData(object)[[1]]))
+  }
+  if(groups == 0) groups == 1
+  return(groups)
+}
