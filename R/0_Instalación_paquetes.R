337d70b997578bc64803005c23a244d9e6e497d6
Jordi Ruf Vilches <jrufv@uoc.edu>
2021-04-19 11:10:59 +0200

    Se añade un nuevo paquete

--- R/0_Instalación_paquetes.R
+++ R/0_Instalación_paquetes.R
@@ -2,15 +2,16 @@
 #'
 #' Instala y carga los paquetes de Bioconductor necesarios para el correcto
 #' funcionamiento del paquete en caso de que no lo estuvieran.
-#' @param annot_pack Paquete de anotaciones para los datos del análisis.
+#' @param annot_pack Paquetes de anotaciones para los datos del análisis.
 #' @return
 #' @export
 #' @examples
 #' ins_pack()
 
 ins_pack <- function(annot_pack = NULL) {
-  packBioc <- c("Biobase", "edgeR", "genefilter", "MSnbase", "oligo",  "oligoClasses",
-                "pmp", "POMA", "S4Vectors", "SummarizedExperiment", "xcms", annot_pack)
+  packBioc <- c("AnnotationDbi", "Biobase", "edgeR", "genefilter", "MSnbase", "oligo",
+                "oligoClasses", "pmp", "POMA", "S4Vectors", "SummarizedExperiment",
+                "xcms", annot_pack)
 
   for(i in 1:length(packBioc)) {
     if(!(require(packBioc[i], character.only = TRUE)))
