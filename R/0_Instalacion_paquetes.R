d7a73fd195a53a5402bfb8e17b25362dd4db8ec5
JORDI RUF VILCHES <jordiruf@gmail.com>
2021-05-17 16:52:46 +0200

    Añade paquetes y el propio paquete

--- R/0_Instalacion_paquetes.R
+++ R/0_Instalacion_paquetes.R
@@ -3,19 +3,22 @@
 #' Instala y carga los paquetes de Bioconductor necesarios para el correcto
 #' funcionamiento del paquete en caso de que no lo estuvieran.
 #' @param annot_pack Paquetes de anotaciones para los datos del análisis.
-#' @return
+#' @return Vacío.
 #' @export
 #' @examples
 #' ins_pack()
 
 ins_pack <- function(annot_pack = NULL) {
-  packBioc <- c("AnnotationDbi", "Biobase", "BiocGenerics", "edgeR", "genefilter",
-                "limma", "MSnbase", "oligo", "oligoClasses", "pmp", "POMA", "ReactomePA",
-                "S4Vectors", "SummarizedExperiment", "xcms", annot_pack)
+  packBioc <- c("AnnotationDbi", "Biobase", "BiocGenerics", "edgeR", "enrichplot",
+                "genefilter", "limma", "MSnbase", "oligo", "oligoClasses", "pmp", "POMA",
+                "ReactomePA", "S4Vectors", "SummarizedExperiment", "xcms", annot_pack)
 
   for(i in 1:length(packBioc)) {
     if(!(require(packBioc[i], character.only = TRUE)))
       BiocManager::install(packBioc[i])
     library(packBioc[i], character.only = TRUE)
   }
+
+  if(!require("TFMjrufv"))
+    devtools::install_github("jrufv/TFMjrufv")
 }
